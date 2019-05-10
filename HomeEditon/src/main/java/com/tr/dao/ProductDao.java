577a3b3a41162665650e488dba7e0d67c6ebd832
MOIPA <tassassin@sina.com>
2019-05-10 11:31:39 +0800

    format

--- HomeEditon/src/main/java/com/tr/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.tr.dao;
-
-import com.tr.domain.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductDao extends JpaRepository<ProductEntity, Integer> {
-
-}
