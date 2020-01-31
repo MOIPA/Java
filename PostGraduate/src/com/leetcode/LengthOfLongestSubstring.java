0e6b4d6764f3001526d365e843a1ebbb246e1a99
rui tang <tassassin@sina.com>
2020-01-31 18:04:29 +0800

    LengthOfLongestSubString

--- PostGraduate/src/com/leetcode/LengthOfLongestSubstring.java
+++ PostGraduate/src/com/leetcode/LengthOfLongestSubstring.java
@@ -8,8 +8,29 @@ public class LengthOfLongestSubstring {
     }
 
     private static class solution {
+        //遍历
         public int lengthOfLongestSubstring(String s) {
-            return 0;
+            if (s.equals(""))return 0;
+            int count = 1;
+            int record = 1;
+            boolean continueFlag = true;
+            for (int i = 0; i < s.length(); i++) {
+                continueFlag = true;
+                count = 1;
+                for (int j = i + 1; j < s.length(); j++) {
+                    // i~j 为集合 j指向的为未添加字符，将未添加字符同集合遍历
+                    for (int k = i; k < j; k++) {
+                        if (s.charAt(k) == s.charAt(j)) {
+                            continueFlag = false;
+                            break;
+                        }
+                    }
+                    if (!continueFlag) break;
+                    count++;
+                    if (count > record) record = count;
+                }
+            }
+            return record;
         }
     }
 }
