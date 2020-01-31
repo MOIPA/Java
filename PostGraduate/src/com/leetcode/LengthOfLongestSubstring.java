60e20536ed926886da2a6dc04bc70c494d010bb2
rui tang <tassassin@sina.com>
2020-01-31 16:38:38 +0800

    ds + algorithm

---
+++ PostGraduate/src/com/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        String testString = "pwwkew";
+        solution solution = new solution();
+        System.out.println(solution.lengthOfLongestSubstring(testString));
+    }
+
+    private static class solution {
+        public int lengthOfLongestSubstring(String s) {
+            return 0;
+        }
+    }
+}
